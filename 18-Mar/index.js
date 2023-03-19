346a0c1796bfa033803780e0cb45d19de712b815
Vishnu Prakash <vishnuprakashvp2512@gmail.com>
2023-03-19 15:34:16 +0530

    Variables

---
+++ 18-Mar/index.js
@@ -0,0 +1,14 @@
+const presentYear = 2023;
+let firstName = "Vishnu";
+let lastName = "Prakash";
+let fullName = firstName + " " + lastName;
+const birthYear = 1999;
+let age = presentYear - birthYear;
+const grade = 5;
+
+const resultString = `My name is ${fullName}. My age is ${age}. I'm studying in ${grade}th grade.`
+console.log(resultString);
+
+
+
+
